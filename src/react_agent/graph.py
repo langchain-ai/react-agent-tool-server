e9a6057292259b54df418245d82ff10477da7ace
Eugene Yurtsev <eyurtsev@gmail.com>
2025-03-06 21:46:35 -0500

    x

--- src/react_agent/graph.py
+++ src/react_agent/graph.py
@@ -20,17 +20,16 @@ async def make_graph(config: RunnableConfig) -> CompiledStateGraph:
         for tool in TOOLBOX.get_tools()
         if tool.name in configuration.selected_tools or not configuration.selected_tools
     ]
-
     # Initialize the model with tool binding. Change the model or add more tools here.
-    model = load_chat_model(configuration.model).bind_tools(tools_to_use)
+    model = load_chat_model(configuration.model)
 
     # Format the system prompt. Customize this to change the agent's behavior.
-    system_message = configuration.system_prompt.format(
+    prompt = configuration.system_prompt.format(
         system_time=datetime.now(tz=UTC).isoformat()
     )
 
     graph = create_react_agent(
-        model, system_message, config_schema=APP_STATE.configurable
+        model, tools=tools_to_use, prompt=prompt, config_schema=APP_STATE.configurable
     )
     graph.name = "ReAct Agent"  # This customizes the name in LangSmith
     return graph
